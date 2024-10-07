1c6f91c682e01d1a02d7d8e5b41f9f9f51b84916
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-07 09:01:41 +0530

    created entire structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
 import React from "react";
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+} from "./sections";
+import Nav from "./components/Nav";
 
 const App = () => {
   return (
     <main className="relative">
-      Nav
-      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-      <section className="padding">popular products</section>
-      <section className="padding">Super Quality</section>
-      <section className="padding-x py-10">Services</section>
-      <section className="padding">Special offer</section>
-      <section className="bg-pale-blue padding">Customer Reviews</section>
-      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+      <Nav/>
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
     </main>
   );
 };
